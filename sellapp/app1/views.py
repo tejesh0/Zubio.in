56f25fc7b735444d6a5fdcf425b9c867baf38f02
tejesh95 <tejeshpapineni95@gmail.com>
2015-02-09 19:51:55 +0530

    make a form using django forms class

--- sellapp/app1/views.py
+++ sellapp/app1/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from datetime import datetime
 from elasticsearch import Elasticsearch
+from django import forms
 
 es = Elasticsearch()
 
@@ -10,9 +11,17 @@ es = Elasticsearch()
 def seller_form(request):
 
 	if request.method =="POST":
-		es.index(index="my-index", doc_type="test-type", id=42, body={"any": "data", "timestamp": datetime.now()})
-
+		#create a form instance and populate with data
+		form = NameForm(request.POST)
+		#check whether form details are valid:
+		if form.is_valid():
+			es.index(index="my-index", doc_type="test-type", id=42, body={"any": "data", "timestamp": datetime.now()})
+			HttpResponse("Yo..!! Your item is attracting lot of buyers!!")
 	else:
-		HttpResponse("this is else block of post request")
+		form = SellerForm()
 
-	return HttpResponse("Yo..!! Your item is atteacting lot of buyers!!")
+	return render(request, 'seller_form.html', {'form':form})
+
+
+class SellerForm(forms.Form):
+	prod_description = forms.TextField(label='Looking for buyers interested in..',max_length=5000)
