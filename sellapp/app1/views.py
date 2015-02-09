103cd6bf88c254ddb8f336e8b5d6b4521f6794d7
tejesh95 <tejeshpapineni95@gmail.com>
2015-02-09 19:18:12 +0530

    create view function to handle form data

--- sellapp/app1/views.py
+++ sellapp/app1/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from datetime import datetime
+from elasticsearch import Elasticsearch
+
+es = Elasticsearch()
+
 
 # Create your views here.
+def seller_form(request):
+
+	if request.method =="POST":
+		es.index(index="my-index", doc_type="test-type", id=42, body={"any": "data", "timestamp": datetime.now()})
+
+	else:
+		HttpResponse("this is else block of post request")
+
+	return HttpResponse("Yo..!! Your item is atteacting lot of buyers!!")
