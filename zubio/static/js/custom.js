acba54a2dfcef117571839f1b876015ca0bf7f45
tejesh95 <tejeshpapineni95@gmail.com>
2015-03-11 22:45:57 +0530

    troll delete

--- zubio/static/js/custom.js
+++
@@ -1,9 +0,0 @@
-$(function() {
-	$('.search-field input').focus(function(){
-		$('.search-field').addClass('red-border');
-	});
-
-	$('.search-field input').focusout(function(){
-		$('.search-field').removeClass('red-border');
-	});
-});
