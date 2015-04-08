68c257fd69f118df10dfd8a5f6a2ec77e6778c25
tejesh95 <tejeshpapineni95@gmail.com>
2015-04-08 06:39:58 +0530

    write partial tests for gym profile form

--- zubio/gym/funcitonal_tests/funcitoal_tests.py
+++ zubio/gym/funcitonal_tests/funcitoal_tests.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
-# import selenium.webdriver
-
-# import unitests
+from selenium import webdriver
+import unittest
+import logging
 
 
 # Create your tests here.
@@ -11,3 +11,35 @@ behavioural test go here....
 coming sooon
 
 """
+
+class GymProfileFormTest(unittest.TestCase):
+	def setUp(self):
+		self.browser = webdriver.Firefox()
+		# self.browser.implicitly_wait(3)
+
+	def tearDown(self):
+		self.browser.quit()
+
+	def test_fill_full_form_and_submits(self):
+		#zubio marketing boy opens browser and enter list form loaction
+		self.browser.get('http://localhost:8000/list')
+
+		#check title of page(funky basic test, it fails here, you lost your cover)
+		self.assertIn('zubio', self.browser.title)
+		print self.browser.title
+		logging.warn("start filling form")
+		#jumbo enters the fitness center name,(less than 100 characters)
+		fitness_center_name = self.browser.find_element_by_id('id_Name_Of_The_Fitness_Center')
+		fitness_center_name.send_keys('Zubio fitness center')
+		self.assertEqual(fitness_center_name.get_attribute('value'),'Zubio fitness center')
+
+		
+
+		self.fail('Finish the test')
+
+
+if __name__ == '__main__':
+	unittest.main()
+
+
+
