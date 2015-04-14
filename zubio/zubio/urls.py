2f6430e03561336858ef4dad76f749cc4d90aab5
tejesh95 <tejeshpapineni95@gmail.com>
2015-04-14 20:56:01 +0530

    article view

--- zubio/zubio/urls.py
+++ zubio/zubio/urls.py
@@ -2,6 +2,9 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 import gym.urls
 import gym.views
+
+from gym.views import ArticleListView
+
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'zubio.views.home', name='home'),
