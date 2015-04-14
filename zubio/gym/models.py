4be8175b6f4a4bec34dfd4fb22a7284f53466290
tejesh95 <tejeshpapineni95@gmail.com>
2015-04-14 21:10:26 +0530

    addd article model

--- zubio/gym/models.py
+++ zubio/gym/models.py
@@ -5,31 +5,31 @@ from django.core.validators import RegexValidator
 # Create your models here.
 
 CHOICE_BOOL = (
-	('Yes','Yes'),
-	('No','No')
+	('Yes', 'Yes'),
+	('No', 'No')
 )
 CHOICE_CATEGORY = (
-	('Gym','Gym'),
+	('Gym', 'Gym'),
 	('Spa', 'Spa'),
 	('Yoga Center', 'Yoga Center'),
 	('Salon', 'Salon')
 )
 
+
 class FitnessListing(models.Model):
 	Name_Of_The_Fitness_Center = models.CharField(max_length=100, default="")
-	Choose_Fitness_Center_category = models.CharField(max_length=20,choices=CHOICE_CATEGORY, default='Gym')
+	Choose_Fitness_Center_category = models.CharField(max_length=20, choices=CHOICE_CATEGORY, default='Gym')
 	Address = models.CharField(max_length=500)
-	phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$', message="Phone number must be entered in the format: '+999999999'")
-	phone_number = models.CharField(default="",max_length=20,validators=[phone_regex])
+	phone_regex = RegexValidator(
+	    regex=r'^\+?1?\d{9,15}$', message="Phone number must be entered in the format: '+999999999'")
+	phone_number = models.CharField(default="", max_length=20, validators=[phone_regex])
 	Email = models.EmailField(default="")
 	# Owners_Name = models.CharField(max_length=50, default="")
 
-	#Timings
+	# Timings
 
-
-	#Pricing
+	# Pricing
 	Monthly = models.CharField(max_length=10, default="")
-
 
 	Membership = models.CharField(max_length=4, choices=CHOICE_BOOL, default='No')
 	AC = models.CharField(max_length=4, choices=CHOICE_BOOL, default='No')
@@ -41,12 +41,26 @@ class FitnessListing(models.Model):
 	Description = models.TextField()
 	Gallery = models.FileField(upload_to='documents/%Y/%m/%d')
 
-
 	def __unicode__(self):
 		return self.fit_name
 
+
 class FitnessListingForm(ModelForm):
+
 	class Meta:
 		model = FitnessListing
 		fields = '__all__'
+
+
+class Article(models.Model):
+
+	title = models.CharField(max_length=100)
+
+
+	class Meta:
+		verbose_name = "Article"
+		verbose_name_plural = "Articles"
+
+	def __str__(self):
+		pass
 
